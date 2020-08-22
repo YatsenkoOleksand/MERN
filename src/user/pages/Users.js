1effa530c5f21f56faa0356d07130d6d5af14b06
Oleksandr Yatsenko <alexanderfsonpu@gmail.com>
2020-08-22 14:48:12 +0200

    UserList props added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "1",
+      name: "Alex",
+      image:
+        "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      places: 3
+    }
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
