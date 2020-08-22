7f53e3b4dd1242fe901c19cee747f1d6297a54b4
Oleksandr Yatsenko <alexanderfsonpu@gmail.com>
2020-08-22 15:11:13 +0200

    UserItem
    Basic visualization of the content

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ import UserList from "../components/UserList";
 const Users = () => {
   const USERS = [
     {
-      id: "1",
+      id: 1,
       name: "Alex",
       image:
         "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
