ab7f38b0140f243bc4f594b6eca58448207ff01b
Oleksandr Yatsenko <alexanderfsonpu@gmail.com>
2020-08-22 17:08:42 +0200

    Navigation for mobile devices

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,10 +3,17 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavigationLinks from "./NavigationLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
   return (
+      <React.Fragment>
+      <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+              <NavigationLinks />
+          </nav>
+      </SideDrawer>
     <MainHeader>
       <button className="main-navigation__menu-btn">
         <span />
@@ -16,10 +23,11 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>
+      <nav className="main-navigation__header-nav">
           <NavigationLinks />
       </nav>
     </MainHeader>
+    </React.Fragment>
   );
 };
 
