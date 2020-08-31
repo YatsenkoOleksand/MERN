27adeeac15806a2a5c6a2efbd8ae2d8efcb7d11c
Oleksandr Yatsenko <alexanderfsonpu@gmail.com>
2020-08-31 17:38:32 +0200

    Get the list of all users, signup, login

---
+++ backend/controllers/users-controller.js
@@ -0,0 +1,59 @@
+const HttpError = require('../models/http-error');
+
+const uuid = require('uuid/v4');
+
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Alex',
+    email: 'test@test.com',
+    password: 'testtest',
+  },
+  {
+    id: 'u2',
+    name: 'Viki',
+    email: 'test@test.com',
+    password: 'testtest',
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const existedEmail = DUMMY_USERS.find(e => e.email === email)
+
+  if(existedEmail){
+      throw new HttpError('Could not create an account, a user with this email is already exists', 401);
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError('Could not identify user', 401);
+  }
+
+  res.json({ message: 'Logged in!' });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
