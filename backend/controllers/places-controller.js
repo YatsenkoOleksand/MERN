842f4487eeea4d002dd9387d77efb22fa4c335d2
Oleksandr Yatsenko <alexanderfsonpu@gmail.com>
2020-09-25 18:33:46 +0200

    User Schema

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -3,20 +3,6 @@ const { validationResult } = require('express-validator');
 
 const Place = require('../models/place');
 
-let DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1',
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
